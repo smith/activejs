ba65c4d2f33dd50f4ad10069042bf553c59c5310
Nathan L Smith <nlloyds@gmail.com>
2009-02-08 13:35:02 -0600

    Disabled catch blocks in tests

--- test/asp/test.js
+++ test/asp/test.js
@@ -44,18 +44,18 @@ var ActiveTest = {
     assert: function assert(condition,note)
     {
         ActiveTest.lastNote = note;
-        try
-        {
+        //try
+        //{
             var pass = !!(typeof(condition) === 'function' ? condition() : condition);
             ++ActiveTest[pass ? 'pass' : 'fail'];
             ActiveTest.log((pass ? 'Pass' : 'Fail') + (note ? ': ' + note : ''));
-        }
-        catch(e)
-        {
-            ++ActiveTest.error;
-            ActiveTest.log('Error' + (note ? ': ' + note : ''));
-            ActiveTest.log(e);
-        }
+        //}
+        //catch(e)
+        //{
+            //++ActiveTest.error;
+            //ActiveTest.log('Error' + (note ? ': ' + note : ''));
+            //ActiveTest.log(e);
+        //}
     },
     run: function run()
     {
@@ -78,19 +78,19 @@ var ActiveTest = {
                 {            
                     stack.push(ActiveSupport.curry(function(test_name){
                         ActiveTest.currentTestName = test_name;
-                        try
-                        {
+                        //try
+                        //{
                             ActiveTest.Tests[group_name][test_name](stack.shift());
-                        }
-                        catch(e)
-                        {
-                            ++ActiveTest.error;
-                            ActiveTest.log('Error after test' + (ActiveTest.lastNote ? ': ' + ActiveTest.lastNote : ''));
-                            ActiveTest.log(e);
-                            var output = '[' + group_name + ' Pass:' + ActiveTest.pass +',Fail:' + ActiveTest.fail + ',Error:' + ActiveTest.error + ']';
-                            ActiveTest.summary.push(output);
-                            ActiveTest.log(output);
-                        }
+                        //}
+                        //catch(e)
+                        //{
+                            //++ActiveTest.error;
+                            //ActiveTest.log('Error after test' + (ActiveTest.lastNote ? ': ' + ActiveTest.lastNote : ''));
+                            //ActiveTest.log(e);
+                            //var output = '[' + group_name + ' Pass:' + ActiveTest.pass +',Fail:' + ActiveTest.fail + ',Error:' + ActiveTest.error + ']';
+                            //ActiveTest.summary.push(output);
+                            //ActiveTest.log(output);
+                        //}
                     },test_name));
                     if(ActiveTest.Tests[group_name].cleanup)
                     {
